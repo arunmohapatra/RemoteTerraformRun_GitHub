0c9065846473f59b80cf4a292588802e98199c4d
arun kumar mohapatra <arun071980@yahoo.com>
2026-08-29 12:54:23 +0530

    testing

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t3.micro"
+  default     = "t2.micro"
 }
 
 variable "availability_zone" {
